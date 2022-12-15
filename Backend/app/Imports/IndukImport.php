0834b3b19533414fd70cab90bd02045590fbb411
Afriza Gilleon Ginting <afriza.gilleon@gmail.com>
2022-12-15 08:35:10 +0700

    first commit and there's bug in progress bar while uploading Surat Permohonan

---
+++ Backend/app/Imports/IndukImport.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Parents;
+use Illuminate\Support\Facades\Auth;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\Importable;
+
+class IndukImport implements ToModel,WithHeadingRow
+{
+    use Importable;
+
+    public function __construct($upt)
+    {
+        $this->upt = $upt;
+    }
+
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        if (!isset($row['nomor_sertifikat'])) {
+            return null;
+        }
+        $user = Auth::user()->id;
+
+        return new Parents([
+            'auhtor' => $user,
+            'address'=> $row['letak_alamat'],
+            'item_name'=> $row['nama_barang'],
+            'large'=> $row['luas_induk'],
+            'certificate_number' => $row['nomor_sertifikat'],
+            'certificate_date'=> \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tanggal_sertifikat']),
+            'asset_value'=> $row['nilai_aset'],
+            'upt' => $this->upt
+        ]);
+    }
+}
